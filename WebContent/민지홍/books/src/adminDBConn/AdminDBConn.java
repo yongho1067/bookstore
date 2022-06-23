ed04ff7281ac2bec0e0957875e4c0947e860a343
Honghong <jihong700@naver.com>
2022-06-23 13:35:40 +0900

    소스

---
+++ WebContent/민지홍/books/src/adminDBConn/AdminDBConn.java
@@ -0,0 +1,29 @@
+package adminDBConn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class AdminDBConn {
+
+private Connection con;
+	
+	public Connection getConnection() {
+		return con;
+	}
+	
+	public AdminDBConn() throws ClassNotFoundException, SQLException {
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "book", "1234");
+		
+		
+	}
+	
+}
+
+
+
+
+
